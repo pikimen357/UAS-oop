3bc848f5a89d5f25d53460a2d5b795d9a29798e3
pikimen357 <vidkyadhipradana2004@mail.ugm.ac.id>
2025-06-01 14:59:12 +0700

    input user

--- src/main/java/uas/oop/database/AppRegister.java
+++ src/main/java/uas/oop/database/AppRegister.java
@@ -2,14 +2,41 @@ package uas.oop.database;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
+import java.security.SecureRandom;
+
 public class AppRegister {
     public static void main(String[] args) {
         // Nonaktifkan warning logger
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "off");
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+
+        System.out.print("Password: ");
+        String password = scanner.nextLine(); // kamu bisa hash ini jika perlu
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Full Name: ");
+        String fullName = scanner.nextLine();
+
+        System.out.print("NIK: ");
+        String nik = scanner.nextLine();
+
+        System.out.print("Phone Number: ");
+        String phone = scanner.nextLine();
+
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+
+        // Buat objek Customer
         Customer customer = new Customer(
-                "erico", "erico356", "customer", "erico@gmail.com",
-                "Erico China", "823428342938", "087435242", "Jl. Ciu No. 123"
+                username, password, "customer", email,
+                fullName, nik, phone, address
         );
 
         try (Connection connection = ConnectionUtil.getDataSource().getConnection()) {
@@ -18,13 +45,24 @@ public class AppRegister {
             customer.insertToUser(connection);
             customer.insertToCustomer(connection);
 
-            long accountNumber = 460300481L;
-            SavingsAccount account = new SavingsAccount(accountNumber, 5_000_000, customer.getIdUser());
+            // Generate account number acak
+            SecureRandom random = new SecureRandom();
+            long accountNumber = 100_000_000_000L + (Math.abs(random.nextLong()) % 900_000_000_000L);
+
+            // Buat akun
+
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Saldo awal : ");
+            double saldo = sc.nextDouble();
+
+            SavingsAccount account = new SavingsAccount(accountNumber, saldo, customer.getIdUser());
             account.insertToAccounts(connection);
 
             connection.commit();
             System.out.println("Registrasi berhasil!");
             System.out.println("Customer ID: " + customer.getIdUser());
+            System.out.println("Account Number: " + account.getAccountNumber());
+            System.out.println("Balance : " + account.getBalance());
 
         } catch (SQLException e) {
             System.out.println("Gagal registrasi: " + e.getMessage());
--- src/main/java/uas/oop/database/AppTransaksi.java
+++ src/main/java/uas/oop/database/AppTransaksi.java
@@ -2,12 +2,23 @@ package uas.oop.database;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class AppTransaksi {
     public static void main(String[] args) {
 
-        String username = "siti_amalia";
-        String passwordInput = "siti123"; // input dari user
+//        Input username
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter username : ");
+        String username = myObj.nextLine();
+
+//        Input password
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter password : ");
+        String passwordInput = scan.nextLine();
+
+//        String username = "siti_amalia";
+//        String passwordInput = "siti123"; // input dari user
         String passwordHashDB = null;
         int accountNumber;
         int idCustomer = -1;
@@ -32,7 +43,7 @@ public class AppTransaksi {
                 if (passwordInput.equals(passwordHashDB)) {
                     System.out.println("Login berhasil. ");
                     System.out.println("Username : " + username);
-//                    System.out.println("Account Number : " + accountNumber);
+                    System.out.println("Account Number : " + accountNumber);
                 } else {
                     System.out.println("Password salah.");
                     return;
@@ -54,18 +65,54 @@ public class AppTransaksi {
         try (Connection connection = ConnectionUtil.getDataSource().getConnection()) {
             connection.setAutoCommit(false);
 
-            SavingsAccount account = SavingsAccount.loadFromDB(accountNumber, connection);
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Type ('deposit' (1) or 'withdrawal' (2)) : ");
+            String type = scanner.nextLine().toLowerCase();
 
-            Transaction t1 = new Transaction(accountNumber, "deposit", 500_000, "Setoran tunai");
-            Transaction t2 = new Transaction(accountNumber, "withdrawal", 200_000, "Tarik tunai");
+            if (type.equals("deposit") || type.equals("1")) {
+                SavingsAccount account = SavingsAccount.loadFromDB(accountNumber, connection);
+                account.showBalance();
 
-            t1.process(account, connection);
-            t2.process(account, connection);
+                Scanner scn = new Scanner(System.in);
+                System.out.print("Amount : ");
+                double amount = scn.nextDouble();
 
-            connection.commit();
-            System.out.println("Semua transaksi berhasil diproses.");
+                Transaction t1 = new Transaction(accountNumber, "deposit", amount, "Setoran tunai");
+                t1.process(account, connection);
 
-            account.showBalance();
+                connection.commit();
+                System.out.println("Deposit sebesar " + amount + " berhasil.");
+
+                account.showBalance();
+            } else if (type.equals("withdrawal") || type.equals("2")) {
+                SavingsAccount account = SavingsAccount.loadFromDB(accountNumber, connection);
+                account.showBalance();
+
+                Scanner scn = new Scanner(System.in);
+                System.out.print("Amount : ");
+                double amount = scn.nextDouble();
+
+                Transaction t1 = new Transaction(accountNumber, "withdrawal", amount, "Tunai");
+                t1.process(account, connection);
+                connection.commit();
+                System.out.println("Transaksi tarik tunai sebesar : " + amount + " berhasil.");
+
+                account.showBalance();
+            } else {
+                System.out.println("jenis transaksi tidak ditemukan.");
+                connection.rollback();
+            }
+
+//            SavingsAccount account = SavingsAccount.loadFromDB(accountNumber, connection);
+//
+//            Transaction t1 = new Transaction(accountNumber, "deposit", 500_000, "Setoran tunai");
+//
+//            t1.process(account, connection);
+//
+//            connection.commit();
+//            System.out.println("Semua transaksi berhasil diproses.");
+
+
         } catch (SQLException e) {
             System.out.println("Kesalahan transaksi: " + e.getMessage());
         }
